c690782d732ac228479103743b9a69a2f7cd4083
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-11-07 09:36:00 +0200

    added star animation

--- data/src/components/Home/Info/Info.jsx
+++ data/src/components/Home/Info/Info.jsx
@@ -3,17 +3,23 @@ import styles from './Info.module.css'
 
 function Info() {
     return (
-        <div className={`${styles["main-info"]}`}>
-            <h1>CustomCADs</h1>
-            <h2 className={`${styles.quote}`}>Design, Create, Innovate</h2>
-            <p>Join our 3D designer platform or order your custom models.</p>
-            <p>From design to 3D print, we've got you covered.</p>
-            <div>
-                <Button link="/register" text="Get Started" className={`${styles.btn}`} />
-                <h2>Or</h2>
-                <Button link="/register" text="Explore" className={`${styles.btn}`} />
+        <>
+            <div className={`${styles["main-info"]}`}>
+                <h1>CustomCADs</h1>
+                <h2 className={`${styles.quote}`}>Design, Create, Innovate</h2>
+                <p>Join our 3D designer platform or order your custom models.</p>
+                <p>From design to 3D print, we've got you covered.</p>
+                <div>
+                    <Button link="/register" text="Get Started" className={`${styles.btn}`} />
+                    <h2>Or</h2>
+                    <Button link="/register" text="Explore" className={`${styles.btn}`} />
+                </div>
+                <div className={`${styles.star}`}>
+                    <div className={`${styles.shadow}`}></div>
+                </div>
             </div>
-        </div>
+           
+        </>
     );
 }
 
--- data/src/layout/Header/Header.jsx
+++ data/src/layout/Header/Header.jsx
@@ -1,10 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUserCog} from '@fortawesome/free-solid-svg-icons';
 import styles from "./Header.module.css"
 
 function Header()
 {
     return (
     <header className={`${styles.header}`}>
-        App Header
+        <FontAwesomeIcon icon={faBars} size="2x" />
+        <FontAwesomeIcon icon={faUserCog} size="2x" />
     </header>
     );
 }
