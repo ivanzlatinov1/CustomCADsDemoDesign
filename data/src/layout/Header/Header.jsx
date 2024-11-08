89e1397552bfb69474ae4b5635d064dd676e1491
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-11-08 08:45:45 +0200

    fixed header

--- data/src/layout/Header/Header.jsx
+++ data/src/layout/Header/Header.jsx
@@ -1,15 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faUserCog} from '@fortawesome/free-solid-svg-icons';
+import { faUserCog } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
 import styles from "./Header.module.css"
 
-function Header()
-{
+function Header() {
     return (
-    <header className={`${styles.header}`}>
-        <FontAwesomeIcon icon={faBars} size="2x" />
-        <FontAwesomeIcon icon={faUserCog} size="2x" />
-    </header>
+        <header className={`${styles.header}`}>
+
+            <div className={`${styles["content-start"]}`}>
+                <FontAwesomeIcon icon={faBars} size="2x" />
+            </div>
+
+            <div className={`${styles["content-middle"]}`}>
+
+            </div>
+
+            <div className={`${styles["content-end"]}`}>
+                <FontAwesomeIcon icon={faCoins} size="2x" />
+                <span>|</span>
+                <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+                <span>|</span>
+                <FontAwesomeIcon icon={faUserCog} size="2x" />
+            </div>
+
+        </header>
     );
 }
 
