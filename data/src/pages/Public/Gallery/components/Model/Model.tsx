fb94225fc168a09ca9f8957de71d67e35f62b0f1
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-11-25 09:53:55 +0200

    made product page

--- data/src/pages/Public/Gallery/components/Model/Model.tsx
+++ data/src/pages/Public/Gallery/components/Model/Model.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import styles from './Model.module.css'
 
 interface ModelProps {
@@ -11,6 +12,16 @@ interface ModelProps {
 }
 
 const Model: React.FC<ModelProps> = ({ model }) => {
+    const navigate = useNavigate();
+
+    const handleDetailsClick = () => {
+        navigate('/product', { state: { model } });
+    };
+
+    const handleBuyNowClick = () => {
+        navigate('/wallet');
+    }
+
     return (
         <div className={`${styles.model}`}>
             <b></b>
@@ -19,9 +30,13 @@ const Model: React.FC<ModelProps> = ({ model }) => {
                 <p className={`${styles.title}`}>{model.name}<br /><span>{model.author}</span></p>
                 <div className={`${styles.button}`}>
                     <div className={`${styles["button-container"]}`}>
-                        <button className={`${styles.btn}`}>
+                        <button onClick={handleDetailsClick} className={`${styles.btn}`}>
                             <span className={`${styles.hover}`}></span>
                             Details
+                        </button>
+                        <button onClick={handleBuyNowClick} className={`${styles.btn}`}>
+                            <span className={`${styles.hover}`}></span>
+                            Buy Now
                         </button>
                     </div>
                 </div>
--- data/src/pages/Public/Product/ProductInfo.tsx
+++ data/src/pages/Public/Product/ProductInfo.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import Transition from "../../../components/Transition/Transition";
+import BtnLink from "../../../components/Button/Button";
+import styles from "./ProductInfo.module.css"
 
-const ProductInfo: React.FC = () => { 
+const ProductInfo: React.FC = () => {
+    const location = useLocation();
+    const { model } = location.state || {};
+
+    if (!model) {
+        return <p>Model not found!</p>;
+    }
+
     return (
-        <>
-        </>
+        <Transition>
+            <div className={`${styles.container}`}>
+                <div className={`${styles.product}`}>
+                    <img src={model.src} alt={model.name} />
+                    <h1>{model.name}</h1>
+                    <p><strong>Role:</strong> {model.role}</p>
+                    <p><strong>Author:</strong> {model.author}</p>
+                    <BtnLink className={`${styles.back}`} text="Go Back" link="/gallery" />
+                </div>
+            </div>
+        </Transition>
     );
 }
 
