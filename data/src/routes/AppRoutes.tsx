c607515582bb91e9a219e6ee272a3c24a4e677dc
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2025-02-01 19:57:10 +0200

    fixed routing issues

--- data/src/routes/AppRoutes.tsx
+++ data/src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Public/Home/Home';
 import Register from '../pages/Guest/Register/Register'
 import ClientRegister from '../pages/Guest/Register/Client/ClientRegister';
@@ -32,41 +32,43 @@ import ProcessorPage from '../pages/Public/Editor/ProcessorPage/ProcessorPage';
 
 const AppRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/register/client" element={<ClientRegister />} />
-      <Route path="/register/contributor" element={<ContributorRegister />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/product" element={<ProductInfo />} />
-      <Route path="/roles" element={<Roles />} />
-      <Route path="/roles/client" element={<ClientRole />} />
-      <Route path="/roles/contributor" element={<ContributorRole />} />
-      <Route path="/roles/designer" element={<DesignerRole />} />
-      {/* <Route path="/about" element={<AboutUs />} /> */}
-      <Route path="/account" element={<Account />} />
-      <Route path="/custom-models" element={<CustomModels />} />
-      <Route path="/order-custom" element={<OrderCustomModel />} />
-      <Route path="/cart" element={<MainCart />} />
-      <Route path="/carts" element={<Carts />} />
-      <Route path="/details" element={<ProductDetails />} />
-      <Route path="/client-dashboard" element={<ClientDashboard />} />
-      <Route path="/order-details" element={<OrderDetails />} />
-      <Route path="/view-cad" element={<ViewCAD />} />
-      <Route path="/shipments" element={<Shipments />} />
-      <Route path="/shipment-details" element={<ShipmentDetails />} />
-      <Route path="/viewer" element={<ViewerPage />} />
-      <Route path="/adjust-dimensions" element={<DimensionsPage />} />
-      <Route path="/processor" element={<ProcessorPage />} />
-      <Route path="/error/400" element={<ErrorPage status={400} />} />
-      <Route path="/error/401" element={<ErrorPage status={401} />} />
-      <Route path="/error/403" element={<ErrorPage status={403} />} />
-      <Route path="/error/404" element={<ErrorPage status={404} />} />
-      <Route path="*" element={<ErrorPage status={404} />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/client" element={<ClientRegister />} />
+        <Route path="/register/contributor" element={<ContributorRegister />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/product" element={<ProductInfo />} />
+        <Route path="/roles" element={<Roles />} />
+        <Route path="/roles/client" element={<ClientRole />} />
+        <Route path="/roles/contributor" element={<ContributorRole />} />
+        <Route path="/roles/designer" element={<DesignerRole />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/custom-models" element={<CustomModels />} />
+        <Route path="/order-custom" element={<OrderCustomModel />} />
+        <Route path="/cart" element={<MainCart />} />
+        <Route path="/carts" element={<Carts />} />
+        <Route path="/details" element={<ProductDetails />} />
+        <Route path="/client-dashboard" element={<ClientDashboard />} />
+        <Route path="/order-details" element={<OrderDetails />} />
+        <Route path="/view-cad" element={<ViewCAD />} />
+        <Route path="/shipments" element={<Shipments />} />
+        <Route path="/shipment-details" element={<ShipmentDetails />} />
+        <Route path="/viewer" element={<ViewerPage />} />
+        <Route path="/adjust-dimensions" element={<DimensionsPage />} />
+        <Route path="/processor" element={<ProcessorPage />} />
+        <Route path="/error/400" element={<ErrorPage status={400} />} />
+        <Route path="/error/401" element={<ErrorPage status={401} />} />
+        <Route path="/error/403" element={<ErrorPage status={403} />} />
+        <Route path="/error/404" element={<ErrorPage status={404} />} />
+        <Route path="*" element={<ErrorPage status={404} />} />
+        {/* <Route path="/about" element={<AboutUs />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
